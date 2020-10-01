63c7042cd2c8c1e9045fb167f28246fbe4b1b952
Kaya Overholt <overholtkaya@gmail.com>
2020-09-30 21:18:18 -0700

    added two questions and a final response

--- js/app.js
+++ js/app.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var correctAnswers = 0;
+
 var userName=prompt('Welcome! What is your name?');
 
 alert('Nice to meet you, ' + userName);
@@ -8,14 +10,14 @@ console.log('Username: ' + userName);
 alert('Pop quiz! Lets see what you know about me!');
 
 var userAnswer1 = prompt('Is my name Kaya?')
-
 var getChar = userAnswer1.charAt(0);
 var finalChar = getChar.toUpperCase();
 
 if(finalChar == 'Y') {
     // console.log('You are correct!');
     alert('You are correct!');
-}if(finalChar == 'N'){
+    correctAnswers++;
+}else if(finalChar == 'N'){
     // console.log('Hey, didn\'t you navigate here from my canvas page... with my name on it?');
     alert('Hey, didn\'t you navigate here from my canvas page... with my name on it?');
 } else {
@@ -23,30 +25,30 @@ if(finalChar == 'Y') {
 }
 
 var userAnswer2 = prompt('Is it true that I have run 4th edition D&D professionally?');
-
 getChar = userAnswer2.charAt(0);
 finalChar = getChar.toUpperCase();
 
 if(finalChar == 'Y') {
     // console.log('Not quite! I only did 3.5 and 5.');
     alert('Not quite! I only did 3.5 and 5.');
-}if(finalChar == 'N'){
+}else if(finalChar == 'N'){
     // console.log('Good job!');
     alert('Good job!');
+    correctAnswers++;
 } else {
     alert('That\'s not a real answer!');
 }
 
 
 var userAnswer3 = prompt('Do I play animal crossing?');
-
 getChar = userAnswer3.charAt(0);
 finalChar = getChar.toUpperCase();
 
 if(finalChar == 'Y') {
     // console.log('Correct!');
-    alert('Correct!')
-}if(finalChar == 'N'){
+    alert('Correct!');
+    correctAnswers++;
+}else if(finalChar == 'N'){
     // console.log('Well..Does 400+ hours of gameplay count...? If so you\'re wrong, sorry.');
     alert('Well... does 400+ hours of gameplay count...? If so you\'re wrong, sorry.');
 } else {
@@ -62,9 +64,50 @@ finalChar = getChar.toUpperCase();
 if(finalChar == 'Y') {
     // console.log('Listen, I made mistakes okay?');
     alert('Listen, I made mistakes okay?');
-}if(finalChar == 'N'){
+    correctAnswers++;
+}else if(finalChar == 'N'){
     // console.log('Incorrect, sorry!');
     alert('Incorrect, sorry!');
 } else {
     alert('That\'s not a real answer!');
 }
+
+var userGuess = prompt('Okay, now guess a number between 1 and 10!');
+var numberAnswer = Math.floor(Math.random( )* 10) + 1;
+for(var i = 0; i < 4; i++){
+    if(userGuess != numberAnswer && i === 3){
+        alert('Out of guesses, better luck next time! The answer was ' + numberAnswer + '.');
+    }else if(userGuess > numberAnswer){
+        var userGuess = prompt('Too big, try again!');
+    }else if(userGuess < numberAnswer){
+        var userGuess = prompt('Too small, try again!');
+    }else if(userGuess == numberAnswer){
+        alert('Good job!');
+        correctAnswers++;
+        break;
+    }
+}
+
+var userGuess2 = prompt('Name a martial D&D class:');
+var acceptableAnswers = ['barbarian', 'fighter', 'monk', 'paladin', 'ranger', 'rogue'];
+var tf = true;
+for(var j = 0; j < 5; j++){
+    for(var k = 0; k < 6; k++){
+        if(userGuess2.toLowerCase() != acceptableAnswers[k]){
+            var tf = false;
+            console.log(userGuess2 + ' ' + acceptableAnswers[k])
+        } else if(userGuess2.toLowerCase() === acceptableAnswers[k]){
+            var tf = true;
+            break;
+        }
+    }
+    if(tf === false){
+        var userGuess2 = prompt('Nope, try again!');
+    } else if(tf === true){
+        alert('Great job!');
+        correctAnswers++;
+        break;
+    }
+}
+
+alert('Thanks for playing, ' + userName + '! You got ' + correctAnswers + 'out of 6.');
